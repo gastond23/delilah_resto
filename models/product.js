9c8926cacc5d1af344ecd99e16724e444e7654d3
Gaston Dandre <gaston.dandre@gmail.com>
2020-10-31 05:35:27 -0300

    modificando function constructor product

--- models/product.js
+++ models/product.js
@@ -1,10 +1,9 @@
-
-
 module.exports = class Product {
-    constructor(name, price, description) {
+    constructor(name, price, description, imageUrl) {
         this.name = name;
         this.price = price;
         this.description = description;
+        this.imageUrl = imageUrl;
     }
     save() {
         products.push(this);
