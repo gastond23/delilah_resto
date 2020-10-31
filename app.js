ba44d4d0f6c7acabb8ef9aa15d7ab0bbcb9ab830
Gaston Dandre <gaston.dandre@gmail.com>
2020-10-31 02:07:12 -0300

    Iniciando router

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+const adminRouter = require('./routes/admin');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(adminRouter);
 
 app.listen(port, () => {
     console.log(`API Server is running on port ${port}.`);
-})
+});
