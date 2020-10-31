355977b28983e2b661c2da5c414a1a0be9679dff
Gaston Dandre <gaston.dandre@gmail.com>
2020-10-31 02:16:02 -0300

    agregado router client

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const port = 3000;
 
 const adminRouter = require('./routes/admin');
+const clientRouter = require('./routes/client');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(adminRouter);
+app.use(clientRouter);
 
 app.listen(port, () => {
     console.log(`API Server is running on port ${port}.`);
