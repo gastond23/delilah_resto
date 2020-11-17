6986b898f2098a2367af0eee6c5dd2f5682b8bbe
Gaston Dandre <gaston.dandre@gmail.com>
2020-11-17 02:04:26 -0300

    Iniciando modelo User y relacionando con Product

--- app.js
+++ app.js
@@ -8,13 +8,22 @@ const sequelize = require('./data/database');
 const adminRouter = require('./routes/admin');
 const clientRouter = require('./routes/client');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/admin', adminRouter);
 app.use(clientRouter);
 
-sequelize.sync().then(result => {
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+//.sync({ force: true })
+.sync()
+.then(result => {
     //console.log(result);
     app.listen(port, () => {
         console.log(`API Server is running on port ${port}.`);
